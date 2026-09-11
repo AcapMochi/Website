68394a762d5fae2f44bfad1b5fbfc9d78b0ed5c0
AcapMochi <asyrafmochi@gmail.com>
2026-09-11 16:53:07 +0800

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="menu">
+        <img src="" alt="logo" width="600" height="400">
+        <button>X</button>
+
+        <h1>START WITH US</h1>
+        <h4><a href="#">HOME</a></h4>
+        <h4><a href="#">CURRENT LISTINGS</a></h4>
+        <h4><a href="#">FEATURED AGENTS</a></h4>
+
+        <h1>OUR STORY</h1>
+        <h4><a href="#">ABOUT US</a></h4>
+        <h4><a href="#">HEAR FROM OUR LEADERS</a></h4>
+        <h4><a href="#">ELHAM MANSIZ</a></h4>
+
+        <h1>CONNECT WITH US</h1>
+        <h4><a href="#">OFFICE LOCATIONS</a></h4>
+        <h4><a href="#">CORPORATE PROFILE</a></h4>
+
+        <h6>elhammansiz28848@gmail.com</h6>
+        <img src="facebook.png">
+        <img src="instagram.png">
+        <img src="tiktok.png">
+    </div>
+</body>
+
+</html>
